5f62682a9054f5a300c312e82cbf89e440d81e7f
sankesh97 <ramojitinorange@gmail.com>
2023-07-09 22:00:53 +0530

    Details Changes

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -18,28 +18,44 @@ const Details = () => {
 
   return (
     <div className='text-center'>
-      <h2 className='text-info'>
-        Top Countries in {currentDestination && currentDestination.name} for
-        your next holiday.
-      </h2>
+      <h2 className='text-info'>{currentDestination.name}</h2>
       <div className='row py-5'>
-        {currentDestination &&
-          currentDestination.destinations.map((destination) => (
-            <div key={destination.id} className='col'>
-              <NavLink to={`/${continent}/${country}/${destination.id}`}>
-                <div
-                  className='card d-flex flex-column justify-content-end p-5 text-white'
-                  style={{
-                    backgroundImage: `url('${destination.image}')`,
-                    backgroundSize: 'cover',
-                    height: '300px',
-                  }}
-                >
-                  <i className='bi bi-geo-alt-fill'></i> {destination.name}
-                </div>
-              </NavLink>
+        {currentDestination && (
+          <>
+            <div className='col-4'>
+              <img src={currentDestination.image} className='img-fluid' />
             </div>
-          ))}
+            <div className='col-8 text-start'>
+              <p>
+                <span className='text-info'>Description:</span>{' '}
+                {currentDestination.description}
+              </p>
+              <p>
+                <span className='text-info'>Ratings:</span>{' '}
+                {currentDestination.ratings}
+              </p>
+              <p>
+                <span className='text-info'>Reviews:</span>{' '}
+                {currentDestination.reviews}
+              </p>
+              <p>
+                <span className='text-info'>Location:</span>{' '}
+                {currentDestination.location}
+              </p>
+              <p>
+                <span className='text-info'>Opening Hours:</span>{' '}
+                {currentDestination.openingHours}
+              </p>
+              <p>
+                <span className='text-info'>Ticket Price:</span>{' '}
+                {currentDestination.ticketPrice}
+              </p>
+              <a href={currentDestination.website} className='text-danger'>
+                Website
+              </a>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
